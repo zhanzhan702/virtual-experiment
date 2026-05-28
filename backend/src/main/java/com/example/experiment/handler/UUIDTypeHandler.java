ae3837c466ee294ef3e5b88dac4040650950776c
zhanzhan <2837420590@qq.com>
2026-05-28 21:35:54 +0800

    feat: 添加 UUIDTypeHandler 类以支持 UUID 数据库类型的处理

---
+++ backend/src/main/java/com/example/experiment/handler/UUIDTypeHandler.java
@@ -0,0 +1,52 @@
+package com.example.experiment.handler;
+
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+
+import java.nio.ByteBuffer;
+import java.sql.*;
+import java.util.UUID;
+
+public class UUIDTypeHandler extends BaseTypeHandler<String> {
+
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, String parameter, JdbcType jdbcType) throws SQLException {
+        UUID uuid = UUID.fromString(
+                parameter.replaceFirst(
+                        "(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})",
+                        "$1-$2-$3-$4-$5"
+                )
+        );
+        ByteBuffer bb = ByteBuffer.wrap(new byte[16]);
+        bb.putLong(uuid.getMostSignificantBits());
+        bb.putLong(uuid.getLeastSignificantBits());
+        ps.setBytes(i, bb.array());
+    }
+
+    @Override
+    public String getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        byte[] bytes = rs.getBytes(columnName);
+        return bytesToUuidString(bytes);
+    }
+
+    @Override
+    public String getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        byte[] bytes = rs.getBytes(columnIndex);
+        return bytesToUuidString(bytes);
+    }
+
+    @Override
+    public String getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        byte[] bytes = cs.getBytes(columnIndex);
+        return bytesToUuidString(bytes);
+    }
+
+    private String bytesToUuidString(byte[] bytes) {
+        if (bytes == null) return null;
+        ByteBuffer bb = ByteBuffer.wrap(bytes);
+        long high = bb.getLong();
+        long low = bb.getLong();
+        UUID uuid = new UUID(high, low);
+        return uuid.toString().replace("-", ""); // 返回无连字符的 32 位字符串
+    }
+}
