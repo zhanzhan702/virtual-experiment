43b2148df75f0086f7649d8af08a6dcab876e12e
linyuhong345-beep <linyuhong345@gmail.com>
2026-06-03 09:44:53 +0800

    0944

--- backend/src/main/java/com/example/experiment/controller/HelloController.java
+++ backend/src/main/java/com/example/experiment/controller/HelloController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from SpringBoot!lyh666789";
+        return "Hello from SpringBoot!lyh66678999999";
     }
 }
