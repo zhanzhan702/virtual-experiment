9d50a14945bce271e581dbea90317cc24e144be8
zhanzhan <2837420590@qq.com>
2026-07-26 22:29:17 +0800

    修复: 修正项目名称为 "virtual-experiment"，调整 Vite 服务器端口为 5173

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,17 +6,14 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+    }
   },
   server: {
-    port: 5174,
+    port: 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
